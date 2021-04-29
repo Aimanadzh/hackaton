154ebeae470e967c7a03eafbac7ba2184773eff6
Akylai31 <akylaj.anarbekova@gmail.com>
2021-04-29 19:55:52 +0600

    мое

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,46 +5,47 @@ import SearchLogo from "../../assets/images/search.svg"
 import { Link } from 'react-router-dom';
 
 
+export default function Header() {
 
-
-
-const Header = () => {
-    return (
-        <header className="headers">
-            <div className="container">
-                <div className="header__logo">
-                    <span>Sign In</span>
-                    <button>Sign Up</button>
-                </div>
-                <div>
-                    <h1 className="headers__title">
-                <img className="navbar__logo" src={Logo} alt="logo"/>
-                </h1>
-                </div>
-                
+return (
+    <header className="headers">
+        <div className="container">
+            <div className="header__logo">
+                <Link to="/login"><span>Sign In</span></Link>
+                <Link to="/signup"><button>Sign Up</button></Link>
             </div>
-            <div className="header__navbar">
-                <div className="hamburger-menu">
-                    <input type="checkbox" id="menu__toggle" />
-                    <label htmlFor="menu__toggle" className="menu__btn">
-                        <span></span>
-                    </label>
-                    <ul className="navbar__menu menu__box">
-                        <li><Link to="" style={{textDecoration: 'none'}}>Jeverly</Link></li>
-                        <li><Link style={{textDecoration: 'none'}}>Watches</Link></li>
-                        <li><Link style={{textDecoration: 'none'}}>Accessories</Link></li>
-                        <li><Link style={{textDecoration: 'none'}}>Decoration</Link></li>
-                        <li><Link style={{textDecoration: 'none'}}>Gift</Link></li>
-                        <li><Link style={{textDecoration: 'none'}}><img className="navbar__search" src={SearchLogo} alt="search" /></Link></li>
-                    </ul>
-                </div>
+            <div>
+                <h1 className="headers__title">
+            <img className="navbar__logo" src={Logo} alt="logo"/>
+            </h1>
             </div>
+            
+        </div>
+        <div className="header__navbar">
+            <div className="hamburger-menu">
+                <input type="checkbox" id="menu__toggle" />
+                <label htmlFor="menu__toggle" className="menu__btn">
+                    <span></span>
+                </label>
+                <ul className="navbar__menu menu__box">
+                    <li><Link to="" style={{textDecoration: 'none'}}>Jeverly</Link></li>
+                    <li><Link style={{textDecoration: 'none'}}>Watches</Link></li>
+                    <li><Link style={{textDecoration: 'none'}}>Accessories</Link></li>
+                    <li><Link style={{textDecoration: 'none'}}>Decoration</Link></li>
+                    <li><Link style={{textDecoration: 'none'}}>Gift</Link></li>
+                    <li><Link style={{textDecoration: 'none'}}><img className="navbar__search" src={SearchLogo} alt="search" /></Link></li>
+                </ul>
+            </div>
+        </div>
 
 
 
 
-        </header>
-    );
-};
+    </header>
+);
 
-export default Header;
+}
+
+
+
+
